92a40dfbc1e9b09867d56a1bf4198f7d55ebe21e
ChastovSlava <crychs142@gmail.com>
2025-02-24 22:44:12 +0300

    fix15

--- tasks/mpi/chastov_v_algorithm_cannon/src/ops_mpi.cpp
+++ tasks/mpi/chastov_v_algorithm_cannon/src/ops_mpi.cpp
@@ -10,55 +10,6 @@
 #include <cmath>
 #include <vector>
 
-namespace {
-int find_compatible_q(int size, int N) {
-  int q = std::floor(std::sqrt(size));
-  while (q > 0) {
-    if (N % q == 0) {
-      break;
-    }
-    --q;
-  }
-  return q > 0 ? q : 1;
-}
-
-void extract_block(const std::vector<double>& matrix, double* block, int N, int K, int block_row, int block_col) {
-  for (int i = 0; i < K; ++i) {
-    for (int j = 0; j < K; ++j) {
-      block[i * K + j] = matrix[(block_row * K + i) * N + (block_col * K + j)];
-    }
-  }
-}
-
-void multiply_blocks(const std::vector<double>& A, const std::vector<double>& B, std::vector<double>& C, int K) {
-  for (int i = 0; i < K; ++i) {
-    for (int j = 0; j < K; ++j) {
-      for (int k = 0; k < K; ++k) {
-        C[i * K + j] += A[i * K + k] * B[k * K + j];
-      }
-    }
-  }
-}
-
-void rearrange_matrix(const std::vector<double>& gathered_blocks, std::vector<double>& final_matrix, int N, int K,
-                      int q) {
-  for (int block_row = 0; block_row < q; ++block_row) {
-    for (int block_col = 0; block_col < q; ++block_col) {
-      int block_rank = block_row * q + block_col;
-      int block_index = block_rank * K * K;
-
-      for (int i = 0; i < K; ++i) {
-        for (int j = 0; j < K; ++j) {
-          int global_row = block_row * K + i;
-          int global_col = block_col * K + j;
-          final_matrix[global_row * N + global_col] = gathered_blocks[block_index + i * K + j];
-        }
-      }
-    }
-  }
-}
-}  // namespace
-
 bool chastov_v_algorithm_cannon_mpi::TestTaskMPI::PreProcessingImpl() {
   if (world_.rank() == 0) {
     auto* first = reinterpret_cast<double*>(task_data->inputs[0]);
@@ -99,146 +50,6 @@ bool chastov_v_algorithm_cannon_mpi::TestTaskMPI::ValidationImpl() {
 }
 
 bool chastov_v_algorithm_cannon_mpi::TestTaskMPI::RunImpl() {
-  int rank = world_.rank();
-  int size = world_.size();
-
-  boost::mpi::broadcast(world_, matrix_size_, 0);
-  boost::mpi::broadcast(world_, total_elements_, 0);
-
-  int q = find_compatible_q(size, matrix_size_);
-  int K = matrix_size_ / q;
-
-  int color = (rank < q * q) ? 1 : MPI_UNDEFINED;
-
-  MPI_Comm new_comm;
-  MPI_Comm_split(world_, color, rank, &new_comm);
-
-  if (color == MPI_UNDEFINED) {
-    return true;
-  }
-
-  boost::mpi::communicator my_world(new_comm, boost::mpi::comm_take_ownership);
-  rank = my_world.rank();
-  size = my_world.size();
-
-  std::vector<double> scatter_A(total_elements_);
-  std::vector<double> scatter_B(total_elements_);
-  if (rank == 0) {
-    int index = 0;
-    for (int block_row = 0; block_row < q; ++block_row) {
-      for (int block_col = 0; block_col < q; ++block_col) {
-        extract_block(first_matrix_, scatter_A.data() + index, matrix_size_, K, block_row, block_col);
-        extract_block(second_matrix_, scatter_B.data() + index, matrix_size_, K, block_row, block_col);
-        index += K * K;
-      }
-    }
-  }
-
-  std::vector<double> local_A(K * K);
-  std::vector<double> local_B(K * K);
-  std::vector<double> local_C(K * K, 0.0);
-  std::vector<double> unfinished_C(total_elements_);
-
-  boost::mpi::scatter(my_world, scatter_A, local_A.data(), K * K, 0);
-  boost::mpi::scatter(my_world, scatter_B, local_B.data(), K * K, 0);
-
-  int row = rank / q;
-  int col = rank % q;
-
-  int send_rank_A = row * q + (col + q - 1) % q;
-  int recv_rank_A = row * q + (col + 1) % q;
-
-  if (send_rank_A >= size || recv_rank_A >= size) {
-    std::cerr << "Invalid rank for send or receive: send_rank=" << send_rank_A << ", recv_rank=" << recv_rank_A
-              << std::endl;
-    return false;
-  }
-
-  int send_rank_B = col + q * ((row + q - 1) % q);
-  int recv_rank_B = col + q * ((row + 1) % q);
-
-  if (send_rank_B >= size || recv_rank_B >= size) {
-    std::cerr << "Invalid rank for send or receive: send_rank=" << send_rank_B << ", recv_rank=" << recv_rank_B
-              << std::endl;
-    return false;
-  }
-
-  for (int i = 0; i < row; ++i) {
-    boost::mpi::request send_request;
-    boost::mpi::request recv_request;
-
-    std::vector<double> temp(local_A.size());
-    send_request = my_world.isend(send_rank_A, 0, local_A.data(), local_A.size());
-    recv_request = my_world.irecv(recv_rank_A, 0, temp.data(), temp.size());
-
-    if (send_request.active() && recv_request.active()) {
-      send_request.wait();
-      recv_request.wait();
-    } else {
-      return false;
-    }
-
-    local_A = temp;
-  }
-
-  for (int i = 0; i < col; ++i) {
-    boost::mpi::request send_request1;
-    boost::mpi::request recv_request1;
-
-    std::vector<double> temp_B(local_B.size());
-    send_request1 = my_world.isend(send_rank_B, 1, local_B.data(), local_B.size());
-    recv_request1 = my_world.irecv(recv_rank_B, 1, temp_B.data(), temp_B.size());
-
-    if (send_request1.active() && recv_request1.active()) {
-      send_request1.wait();
-      recv_request1.wait();
-    } else {
-      return false;
-    }
-
-    local_B = temp_B;
-  }
-
-  multiply_blocks(local_A, local_B, local_C, K);
-
-  for (int iter = 0; iter < q - 1; ++iter) {
-    boost::mpi::request send_request2;
-    boost::mpi::request recv_request2;
-
-    boost::mpi::request send_request3;
-    boost::mpi::request recv_request3;
-
-    std::vector<double> temp_A(local_A.size());
-    send_request2 = my_world.isend(send_rank_A, 0, local_A.data(), local_A.size());
-    recv_request2 = my_world.irecv(recv_rank_A, 0, temp_A.data(), temp_A.size());
-
-    std::vector<double> temp_B(local_B.size());
-    send_request3 = my_world.isend(send_rank_B, 1, local_B.data(), local_B.size());
-    recv_request3 = my_world.irecv(recv_rank_B, 1, temp_B.data(), temp_B.size());
-
-    if (send_request2.active() && recv_request2.active() && send_request3.active() && recv_request3.active()) {
-      send_request2.wait();
-      recv_request2.wait();
-      send_request3.wait();
-      recv_request3.wait();
-    } else {
-      return false;
-    }
-
-    local_A = temp_A;
-    local_B = temp_B;
-
-    multiply_blocks(local_A, local_B, local_C, K);
-  }
-
-  boost::mpi::gather(my_world, local_C.data(), local_C.size(), unfinished_C, 0);
-  if (rank == 0) {
-    rearrange_matrix(unfinished_C, result_matrix_, matrix_size_, K, q);
-  }
-  return true;
-}
-
-/*bool chastov_v_algorithm_cannon_mpi::TestTaskMPI::RunImpl() {
   int rank = world_.rank();
   int size = world_.size();
 
@@ -413,7 +224,7 @@ bool chastov_v_algorithm_cannon_mpi::TestTaskMPI::RunImpl() {
     }
   }
   return true;
-}*/
+}
 
 bool chastov_v_algorithm_cannon_mpi::TestTaskMPI::PostProcessingImpl() {
   if (world_.rank() == 0) {
